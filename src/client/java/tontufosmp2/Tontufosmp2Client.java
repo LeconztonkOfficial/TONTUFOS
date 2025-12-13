e27eeaa60caf6cba43a73976af0843586350e708
brianchatran <65400978+brianchatran@users.noreply.github.com>
2025-12-13 01:22:35 +0100

    Cambio a FlyingItemEntityRenderer para que se vea la pocion mientras cae

--- src/client/java/tontufosmp2/Tontufosmp2Client.java
+++ src/client/java/tontufosmp2/Tontufosmp2Client.java
@@ -3,6 +3,7 @@ package tontufosmp2;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 import tontufosmp2.entities.ModEntities;
 import tontufosmp2.client.renderer.InvisibleEntityRenderer;
 
@@ -18,9 +19,11 @@ public class Tontufosmp2Client implements ClientModInitializer {
         );
 
 
+        // Registra el renderizador para la entidad de luz temporal
+        // Hace que lo que tires se vea mientras va cayendo, como si fuese una bola de nieve en el juego
         EntityRendererRegistry.register(
                 ModEntities.THROWABLE_LIGHT,
-                ctx -> new InvisibleEntityRenderer<>(ctx)
+                FlyingItemEntityRenderer::new
         );
 
 
